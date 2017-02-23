56cb7bde717290cb270766da7d8bde706ad24e0e
David Morand <david.morand@nuecho.com>
2017-02-22 22:26:15 -0500

    added Polgo.Move struct + unit tests

---
+++ test/models/move_test.exs
@@ -0,0 +1,26 @@
+defmodule Polgo.MoveTest do
+  use ExUnit.Case, async: true
+
+  alias Polgo.Move
+
+     # The "test" macro is imported by ExUnit.Case
+     test "white" do
+       white = Move.white(0, 1)
+       assert white.x == 0
+       assert white.y == 1
+       assert white.color == :white
+     end
+
+     test "black" do
+       black = Move.black(2, 3)
+       assert black.x == 2
+       assert black.y == 3
+       assert black.color == :black
+     end
+
+     test "invalid" do
+       assert_raise FunctionClauseError, fn ->
+         Move.white(8, 8)
+       end
+     end
+end
